d68c7139b42e53cbedb2e34e8317d99dbffc4b6f
kolamor <kolamorev@mail.ru>
2019-03-28 04:08:22 +0300

    rutor_find, micros_market

---
+++ micros_market.py
@@ -0,0 +1,187 @@
+import asyncio
+import aiohttp
+import csv
+from bs4 import BeautifulSoup
+
+primary_url = 'https://marketplace.xbox.com'
+prefix = '/ru-RU/Games/Xbox360Games'
+headers = {
+	'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'
+	}
+
+
+
+class Clien():
+
+	def __init__(self, headers, session=None):
+		self.headers = headers
+		if session is None:
+			self.session = aiohttp.ClientSession(headers=self.headers)
+		else:
+			self.session = session
+
+
+	async def parse(self, url):
+		data = await self._fetch(url)
+		return data
+
+
+	async def _fetch(self,send):
+
+		print(send)
+		async with self.session.get(send) as response:
+			print('-----', response)
+			if response.content_type == 'text/html':
+				data = await response.text()
+			if response.content_type == 'application/json':
+				data =  await response.json()
+			if response.content_type == 'text/xml':
+				data = await response.text()
+			if response.content_type == 'image/jpeg':
+				data = await response.read()
+			if response.content_type == 'image/png':
+				data = await response.read()
+
+			return data
+
+	async def close(self):
+		await self.session.close()
+
+	async def __aenter__(self):
+		return self
+
+	async def __aexit__(self, exception_type, exception_value, traceback):
+		await self.close()
+
+class SaveSvg():
+
+	@classmethod
+	async def unzip_multi(cls, data):
+		with open('xbox360market.csv', 'a') as f:
+			for tup in data:
+				await cls.save_svg(f,tup)
+
+	@classmethod
+	async def save_svg(cls,f, data):
+		# print(data)
+		# with open('rutor_xbox360.csv', 'a') as f:
+		writer = csv.writer(f)
+		writer.writerow( ( data[0],
+                        data[1],
+                        data[2],
+                        ))
+
+class SaveImg():
+
+	def __init__(self, client):
+		self.client = client
+		self.count = 0
+		self.qwe = []
+
+
+	async def get(self, data):
+		try:
+			binarf = await self.client.parse(data[2])
+			self.count +=1
+		except Exception:
+			print('img!!')
+			binarf = b''
+		self.qwe.append((data[0], binarf))
+
+
+	async def sv(self):
+		for data in self.qwe:
+			f = open('img/'+ data[0]+'.jpg', "wb")
+			f.write(data[1])
+			f.close()
+
+
+	async def save(self, data):
+		print(data[2])
+
+		try:
+			binarf = await self.client.parse(data[2])
+			self.timer +=1
+		except Exception:
+			print('img!!')
+			binarf = b''
+		f = open('img/'+ data[0]+'.jpg', "wb")
+		f.write(binarf)
+		f.close()
+
+class Bs:
+
+	def __init__(self, *args, **kwargs):
+	    self.list_next_page = [primary_url+prefix]
+	    self.body_titles = []
+
+	async def get_next_pages(self, html):
+		url_pages = []
+		soup = BeautifulSoup(html, 'lxml')
+		pages = soup.find('div', id='bodycolumn').find('div', id='BodyContent')
+		pages = pages.find('div', class_='Paging').find('a', class_='Next')
+		next_page = str(pages).split('"')[3]
+		self.list_next_page.append(primary_url+next_page)
+		return primary_url+next_page
+
+	async def get_content(self, html):
+		soup = BeautifulSoup(html, 'lxml')
+		product = soup.find('div', id='bodycolumn').find('div', id='BodyContent')
+		# print(product)
+		product = product.find('ol', class_='ProductResults GameTiles').find_all('li')
+		for pr in product:
+			body = str(pr.find('a')).split('"')
+			title = body[13]
+			url = body[1]
+			image = body[9]
+			self.body_titles.append((title, url, image))
+		print(title, url, image)
+		return self.body_titles
+
+
+
+
+async def main():
+	url = primary_url+prefix
+	bs = Bs()
+	async with Clien(headers=headers) as cl:
+		while True:
+			try:
+				data = await cl.parse(url)
+				url = await bs.get_next_pages(data)
+				asyncio.sleep(1)
+				print(url)
+			except Exception:
+				print('dadam')
+				break
+
+		for page in bs.list_next_page:
+			data = await cl.parse(page)
+			await bs.get_content(data)
+
+		# await SaveSvg.unzip_multi(bs.body_titles)
+		save_pic = SaveImg(cl)
+		tasks = []
+		print(len(bs.body_titles))
+		for tit in bs.body_titles:
+			task = loop.create_task(save_pic.get(tit))
+			# task = loop.create_task(save_pic.save(tit))
+			tasks.append(task)
+		await asyncio.gather(*tasks)
+		await save_pic.sv()
+	print(save_pic.count)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	task = [
+            # loop.create_task(main3()),
+            loop.create_task(main())
+		]
+	wait_tasks = asyncio.wait(task)
+	loop.run_until_complete(wait_tasks)
---
+++ rutor_find.py
@@ -0,0 +1,168 @@
+import asyncio
+import aiohttp
+import csv
+from bs4 import BeautifulSoup
+import time
+
+rutor_url = 'http://mega-bit.ga'
+all_words_prefix = '/search/0/0/100/0/'
+any_words_prefix = '/search/0/0/200/0/'
+logic_prefix = '/search/0/0/300/0/'
+
+search = 'assassins creed'
+
+SAVE_RESULT = True
+file_save = 'find_rutor.csv'
+
+
+
+class Client():
+
+	def __init__(self, headers='', session=None):
+		self.headers = headers
+		if session is None:
+			self.session = aiohttp.ClientSession(headers=self.headers)
+		else:
+			self.session = session
+
+
+	async def send(self, url):
+		data = await self._fetch(url)
+		return data, url
+
+	async def get_all(self, urls):
+		tasks = []
+		for url in urls:
+			task = loop.create_task(self._fetch(url))
+			tasks.append(task)
+			all_data = await asyncio.gather(*tasks)
+		return all_data
+
+
+
+	async def _fetch(self,send):
+
+		print(send)
+		async with self.session.get(send) as response:
+			print('-----', response)
+			if response.content_type == 'text/html':
+				data = await response.text()
+			if response.content_type == 'application/json':
+				data =  await response.json()
+			if response.content_type == 'text/xml':
+				data = await response.text()
+			if response.content_type == 'image/jpeg':
+				data = await response.read()
+			if response.content_type == 'image/png':
+				data = await response.read()
+
+		return data
+
+	async def close(self):
+		await self.session.close()
+
+	async def __aenter__(self):
+		return self
+
+	async def __aexit__(self, exception_type, exception_value, traceback):
+		await self.close()
+
+
+class Bs:
+
+	async def get_total_pages(self, html, send):
+		# print(html)
+		url_pages = []
+		soup = BeautifulSoup(html, 'lxml')
+		pages = soup.find('div', id='index').find('b').find_all('a')
+		if len(pages) == 0:
+			return [send,]
+
+		for page in pages:
+			url = str(page).split('"')[1]
+			url_pages.append(rutor_url+url)
+		url_pages.insert(0,send)
+		return url_pages
+
+
+	async def get_full_url_pages(self,pages):
+		full = [ rutor_url+a for a in pages]
+		return full
+
+
+	async def get_content(self, html):
+		soup = BeautifulSoup(html, 'lxml')
+		urls = soup.find('div', id='index').find('table').find_all('tr')
+		del urls[0]
+		if len(urls) == 0:
+			print('не найдено')
+			return []
+		result=[]
+		for a in urls:
+			soup = BeautifulSoup(str(a), 'lxml')
+			b = soup.find_all('a' )
+			self_url = str(b[0]).split('"')[3]
+			magnet = str(b[1]).split('"')[1]
+			url_resurs = str(b[2]).split('"')[1]
+			name = str(b[2]).split('"')[2][1:-5]
+			res = (name, self_url, magnet, url_resurs )
+			result.append(res)
+			# print("---",name)
+			# print("---",self_url)
+			# print("---",magnet)
+			# print("---", url_resurs )
+		return result
+
+	async def get_urls_content(self, html_list):
+		t1 = time.clock()
+		tasks = []
+		for html in html_list:
+			task = loop.create_task(self.get_content(html))
+			tasks.append(task)
+		data = await asyncio.gather(*tasks)
+		t2 = time.clock() - t1
+		print('a',t2)
+		return data
+
+
+class Save:
+
+	@classmethod
+	async def save_csv(cls, data_ar):
+		with open(file_save, 'a') as f:
+			for data_list in data_ar:
+				for data in data_list:
+					writer = csv.writer(f)
+					writer.writerow( ( data[0],
+			                        data[1],
+			                        data[2],
+			                        data[3]) )
+
+
+
+async def main():
+	send = rutor_url+all_words_prefix+search
+	async with Client() as cl:
+
+		html, send = await cl.send(send)
+		bs = Bs()
+		total_pages = await bs.get_total_pages(html, send)
+		data = await cl.get_all(total_pages)
+		urls_pages = await bs.get_urls_content(data)
+		print(urls_pages)
+		if SAVE_RESULT:
+			await Save.save_csv(urls_pages)
+
+
+class NotFound(Exception):
+	pass
+
+
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	task = [
+		loop.create_task(main())
+		]
+	wait_tasks = asyncio.wait(task)
+	loop.run_until_complete(wait_tasks)
+
